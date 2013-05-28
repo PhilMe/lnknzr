509a8ec98a61e62a3020324e787b34daa088f2b2
Philipp Thiel <philipp.thiel@wamf.de>
2013-05-28 16:11:22 +0200

    first lnknzr

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lnknzr::Application.config.secret_token = '3188887f7d85af36cf7ae292597315b0afac88b8a1a4c2c9e6daf81ea1829e358046787813bcb0799741eca3c2212a4c47dff75034c2d2f79f6bab907f34f7f0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Lnknzr::Application.config.session_store :cookie_store, key: '_lnknzr_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Lnknzr::Application.config.session_store :active_record_store
